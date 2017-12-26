f0ddab165cfdc1cf472dd01a1770a95affae09ac
zg56 <zg56@drexel.edu>
2017-12-26 13:42:43 +0100

    Add files via upload

---
+++ ML/Closed-Form-Linear-Regression/ClosedFormLR.m
@@ -0,0 +1,39 @@
+%% CS 383 Zoltan Gercsak Winter 2017 HW 3
+
+clc;
+close all;
+clear all;
+
+% read data file
+filename = 'x06Simple.csv';
+data = csvread(filename, 1);
+data = data(:, 2:end);
+
+% randomize the data
+rng(0);
+data = data(randperm(size(data,1)),:);
+
+% create training and testing set
+trainSet = ceil(size(data,1)*2/3);
+training = data(1:trainSet, :);
+trainFeatures = training(:, 1:end-1);
+trainLabel = training(:, end);
+
+testSet = ceil(size(data,1)*2/3)+1;
+testing = data(testSet:end, :);
+testFeatures = testing(:, 1:end-1);
+testLabel = testing(:, end);
+
+%% Closed form linear regression
+[weight, meanValue, stdValue] = CFLR(trainFeatures, trainLabel);
+
+%% Calculate error
+
+normTestData = (testFeatures - repmat(meanValue, size(testFeatures,1), 1)) ./ repmat(stdValue, size(testFeatures,1), 1);
+normTestData = [ones(size(normTestData,1),1), normTestData];
+prediction = normTestData * weight;
+mse = mean((testLabel - prediction).^2);
+rmse = sqrt(mse);
+
+%% Print out result
+fprintf('Model y = %f + %fx1 + %fx2, RMSE = %f\n', weight(1), weight(2), weight(3), rmse);
